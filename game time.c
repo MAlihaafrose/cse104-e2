3c5a936780c12044f4a626071fe290e23fa718d2
MAlihaafrose <141204409+MAlihaafrose@users.noreply.github.com>
2023-09-30 22:15:31 +0600

    Add files via upload

---
+++ game time.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    if(a==b)printf("O JOGO DUROU 24 HORA(S)\n");
+    else if(b>a)printf("O JOGO DUROU %d HORA(S)\n",b-a);
+    else if(a>b)printf("O JOGO DUROU %d HORA(S)\n",(b+24)-a);
+    return 0;
+}
