bef312c0331afa151d2d6775a8c704e23cc37063
MAlihaafrose <141204409+MAlihaafrose@users.noreply.github.com>
2023-10-01 01:31:08 +0600

    Add files via upload

--- simple sum.c
+++ simple sum.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 int main() {
-
-    int A, B, SOMA;
-    scanf("%d", &A);
+    int A,B,SOMA;
+    scanf("%d",&A);
     scanf("%d",&B);
     SOMA = A+B;
-    printf("SOMA = %d\n", SOMA);
+    printf("SOMA = %d\n",SOMA);
+
+
+
 
     return 0;
 }
