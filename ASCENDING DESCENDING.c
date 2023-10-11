4afcaebc0ceb399e2af1604a6ff3de61bd22a91e
MAlihaafrose <141204409+MAlihaafrose@users.noreply.github.com>
2023-10-11 23:58:22 +0600

    Add files via upload

---
+++ ASCENDING DESCENDING.c
@@ -0,0 +1,22 @@
+ASCENDING DESCENDING
+
+#include<stdio.h>
+
+int main()
+{
+    int x,y;
+    while(1)
+    {scanf("%d",&x);
+   scanf("%d",&y);
+   if(x==y)
+   break;
+   else if(x>y)
+   {printf("Decreascente\n");
+   }
+   else
+   {printf("Crescente\n");
+   }
+   }
+
+return 0;
+}
