cba5abd32f7b4f97e54553f8185480dd1b37ea8f
MAlihaafrose <141204409+MAlihaafrose@users.noreply.github.com>
2023-10-11 23:57:45 +0600

    Add files via upload

---
+++ FIXED PASSWORD.c
@@ -0,0 +1,17 @@
+FIXED PASSWORD
+
+#include<stdio.h>
+
+int main()
+{
+    int password;
+    while(1)
+    {scanf("%d",&password);
+    if(password==2002)
+    {printf("Acesso Permitido\n");
+    break;}
+    else {printf("Senha Invalido\n");
+    }}
+
+return 0;
+}
